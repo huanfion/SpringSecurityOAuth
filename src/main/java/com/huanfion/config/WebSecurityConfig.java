551740c7b7a7221ad77f6fe4a2ff8aa6431a5cb9
huanfion <648890146@qq.com>
2020-07-31 08:14:25 +0800

    使用BCryptPasswordEncoder 加密密码

--- src/main/java/com/huanfion/config/WebSecurityConfig.java
+++ src/main/java/com/huanfion/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -17,14 +18,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager=new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("huanfion").password("123456").authorities("p1").build());
-        manager.createUser(User.withUsername("admin").password("123456").authorities("p2").build());
+        manager.createUser(User.withUsername("huanfion").password(passwordEncoder().encode("123456")).authorities("p1").build());
+        //信息若是没有设置角色授权，就会报错误 java.lang.IllegalArgumentException: Cannot pass a null GrantedAuthority collection
+        manager.createUser(User.withUsername("admin").password(passwordEncoder().encode("123456")).authorities("p2").build());
         return manager;
     }
     //密码编码器
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
     /**
      * 安全拦截机制（最重要）
